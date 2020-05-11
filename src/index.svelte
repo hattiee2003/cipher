840c23fa70d22b601a9d830f55186fe15b4fbbf2
hattiee2003 <50850607+hattiee2003@users.noreply.github.com>
2020-05-11 14:56:04 +1200

    Version 1
    Wrote code so that user can type in an unput then click the encrypt button and the text is outputted as numbers. Tested "testing", "word", "aaaaaa" and "1234"
    Passes all tests

--- src/index.svelte
+++ src/index.svelte
@@ -1,3 +1,31 @@
-<h1>Svelte</h1>
+<script>
+  let input = "";
+  let output = "";
 
-<p>Welcome to coding with Svelte!</p>
+  function encrypt() {
+    console.log("Encrypt button clicked");
+    let plainText = input;
+    let cipherText = "";
+
+    for (let i = 0; i < plainText.length; i++) {
+      cipherText += plainText.charCodeAt(i) + "|";
+    }
+
+    output = cipherText;
+  }
+</script>
+
+<section class="section content">
+  <h1>Cipher</h1>
+
+  <label>
+    Text:
+    <input type="text" bind:value={input} />
+  </label>
+
+  <button on:click={encrypt}>Encrypt</button>
+
+  <h2>Result:</h2>
+
+  <p>{output}</p>
+</section>
